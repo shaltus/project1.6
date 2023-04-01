3a715acb92fcd2bb4ae550705e51d347cfce2e48
shangitbaev <a.shangitbaev@cft.ru>
2023-04-01 16:18:56 +0500

    commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+
   const menuOpen = document.querySelector('.menu-open')
+  const content = document.querySelector('.content')
   const menu = document.querySelector('.aside')
   const menuClose = menu.querySelector('.aside__header-close')
   const overlay = document.querySelector('.overlay')
@@ -7,16 +8,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const openMenu = function () {
     menu.classList.add('aside--active')
     overlay.classList.add('overlay--active')
+    content.classList.add('content--no-scroll')
   }
 
   const closeMenu = function () {
     menu.classList.remove('aside--active')
     overlay.classList.remove('overlay--active')
+    content.classList.remove('content--no-scroll')
   }
-
+  const handleKeyDown = function (event) {
+    if (event.key === 'Escape') {
+      closeMenu()
+    }
+  }
   menuOpen.addEventListener('click', openMenu)
 
   menuClose.addEventListener('click', closeMenu)
 
   overlay.addEventListener('click', closeMenu)
-})
+  document.addEventListener('keydown', handleKeyDown)
+
+
--- src/js/modal.js
+++ src/js/modal.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const modalCall = document.querySelector('.modal-call')
   const modalCallClose = document.querySelector('.modal-form__close--call')
   const menu = document.querySelector('.aside')
+  const content = document.querySelector('.content')
   const overlay = document.querySelector('.overlay')
 
   const openModalCall = function () {
     modalCall.classList.add('modal-call--active')
     overlay.classList.add('overlay--active')
+    content.classList.add('content--no-scroll')
 
     if (menu.classList.contains('aside--active')) {
       menu.classList.remove('aside--active')
@@ -17,6 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeModalCall = function () {
     modalCall.classList.remove('modal-call--active')
     overlay.classList.remove('overlay--active')
+    content.classList.remove('content--no-scroll')
+  }
+  const handleKeyDown = function (event) {
+    if (event.key === 'Escape') {
+      closeModalCall()
+    }
   }
 
   modalCallOpen.forEach((btn) => {
@@ -25,20 +33,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
   modalCallClose.addEventListener('click', closeModalCall, false)
   overlay.addEventListener('click', closeModalCall, false)
+  document.addEventListener('keydown', handleKeyDown)
 })
 
 document.addEventListener('DOMContentLoaded', () => {
   const modalFeedbackOpen = document.querySelectorAll('.menu-btn--chat')
   const modalFeedback = document.querySelector('.modal-feedback')
-  const modalFeedbackClose = document.querySelector(
-    '.modal-form__close--feedback'
-  )
+  const modalFeedbackClose = document.querySelector('.modal-form__close--feedback')
   const menu = document.querySelector('.aside')
+  const content = document.querySelector('.content')
   const overlay = document.querySelector('.overlay')
 
   const openModalCall = function () {
     modalFeedback.classList.add('modal-feedback--active')
     overlay.classList.add('overlay--active')
+    content.classList.add('content--no-scroll')
+    
 
     if (menu.classList.contains('aside--active')) {
       menu.classList.remove('aside--active')
@@ -48,6 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeModalFeedback = function () {
     modalFeedback.classList.remove('modal-feedback--active')
     overlay.classList.remove('overlay--active')
+    content.classList.remove('content--no-scroll')
+  }
+  const handleKeyDown = function (event) {
+    if (event.key === 'Escape') {
+      closeModalFeedback()
+    }
   }
 
   modalFeedbackOpen.forEach((btn) => {
@@ -57,4 +73,6 @@ document.addEventListener('DOMContentLoaded', () => {
   modalFeedbackClose.addEventListener('click', closeModalFeedback, false)
 
   overlay.addEventListener('click', closeModalFeedback, false)
+  document.addEventListener('keydown', handleKeyDown)
 })
+
