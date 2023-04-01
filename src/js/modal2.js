bb842b612cccdb23c54c2f1505cd2ba1d3737884
shangitbaev <a.shangitbaev@cft.ru>
2023-04-01 17:48:24 +0500

    inital commit

--- src/js/modal2.js
+++ src/js/modal2.js
@@ -1,41 +1,37 @@
-
-  const modalCallOpen = document.querySelectorAll('.menu-btn--call')
-  const modalCall = document.querySelector('.modal-call')
-  const modalCallClose = document.querySelector('.modal-form__close--call')
+  const modalFeedbackOpen = document.querySelectorAll('.menu-btn--chat')
+  const modalFeedback = document.querySelector('.modal-feedback')
+  const modalFeedbackClose = document.querySelector('.modal-form__close--feedback')
   const menu = document.querySelector('.aside')
   const content = document.querySelector('.content')
   const overlay = document.querySelector('.overlay')
 
   const openModalCall = function () {
-    modalCall.classList.add('modal-call--active')
+    modalFeedback.classList.add('modal-feedback--active')
     overlay.classList.add('overlay--active')
     content.classList.add('content--no-scroll')
+    
 
     if (menu.classList.contains('aside--active')) {
       menu.classList.remove('aside--active')
     }
   }
 
-  const closeModalCall = function () {
-    modalCall.classList.remove('modal-call--active')
+  const closeModalFeedback = function () {
+    modalFeedback.classList.remove('modal-feedback--active')
     overlay.classList.remove('overlay--active')
     content.classList.remove('content--no-scroll')
   }
   const handleKeyDown = function (event) {
     if (event.key === 'Escape') {
-      closeModalCall()
+      closeModalFeedback()
     }
   }
 
-  modalCallOpen.forEach((btn) => {
+  modalFeedbackOpen.forEach((btn) => {
     btn.addEventListener('click', openModalCall, false)
   })
 
-  modalCallClose.addEventListener('click', closeModalCall, false)
-  overlay.addEventListener('click', closeModalCall, false)
-  document.addEventListener('keydown', handleKeyDown)
-
-
   modalFeedbackClose.addEventListener('click', closeModalFeedback, false)
 
-
+  overlay.addEventListener('click', closeModalFeedback, false)
+  document.addEventListener('keydown', handleKeyDown)
